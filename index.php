32b2931551dd24a880ef9a5c3c15d48e4a88294b
Ryuqing~ <fachru2006@gmail.com>
2023-09-29 11:19:38 +0700

    HxH

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Shooter</title>
+		
+		<style> *{-webkit-tap-highlight-color: rgba(0, 0, 0, 0);}
+				 body {
+					padding: 0;
+					margin: 0;
+				 }
+		</style>
+
+		<script src="shmup.js"></script>
+		
+	</head>
+	<body onload="Main();">
+		<canvas id="Shooter" width="550" height="1000">
+			<audio id="boss_0" preload="auto"  type="audio/mpeg" src="boss.mp3"></audio>
+
+			<audio id="explo_0" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_1" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_2" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_3" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_4" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_5" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_6" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_7" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_8" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+			<audio id="explo_9" preload="auto"  type="audio/mpeg" src="explo.mp3"></audio>
+
+			<audio id="shot_0" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_1" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_2" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_3" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_4" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_5" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_6" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_7" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_8" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+			<audio id="shot_9" preload="auto"  type="audio/mpeg" src="shot.mp3"></audio>
+	</body>
+</html>
